e2e594cc2eed15d041449c995457b2d45f792bf9
a.h.kadar <a.h.kadar@gmail.com>
2021-03-13 19:21:22 +0100

    Intermediate

--- DualImageViewer/main.py
+++ DualImageViewer/main.py
@@ -1,5 +1,5 @@
-from PyQt5 import QtWidgets
-from main_ui import Ui_imageViewer
+from PyQt5 import QtWidgets, QtGui
+from main_ui import Ui_MainWindow
 import sys
 
 
@@ -10,13 +10,14 @@ class DualImageViewer(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)  # Pass all arguments so that it behaves in every way like a QMainWindow
 
         # Setup UI on the ImageViewer instance which is also a QMainWindow instance
-        self.ui = Ui_imageViewer()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         # Connect signals with slots
         self.connect_signals_and_slots()
 
     def connect_signals_and_slots(self):
+        self.ui.actionOpen_Image.triggered.connect(self.open)
         self.ui.actionAbout.triggered.connect(self.about)
         self.ui.actionAbout_Qt.triggered.connect(QtWidgets.qApp.aboutQt)
 
@@ -25,6 +26,33 @@ class DualImageViewer(QtWidgets.QMainWindow):
                           "<p>The <b>Dual Image Viewer</b> example shows how two images "
                           "can be shown in sync such that an operation (zoom, fit to window etc.)"
                           " applied to one image is automatically applied to the other </p>")
+
+    def open(self):
+        options = QtWidgets.QFileDialog.Options()
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
+                                                  'Images (*.png *.jpeg *.jpg *.bmp *.gif)', options=options)
+        if file_name:
+            print(file_name)
+            image = QtGui.QImage(file_name)
+            if image.isNull():
+                QtWidgets.QMessageBox.information(self, "Dual Image Viewer", "Cannot load %s." % file_name)
+                return
+
+            self.ui.imageLabelLeft.setPixmap(QtGui.QPixmap.fromImage(image))
+            self.ui.imageLabelLeft.setBackgroundRole(QtGui.QPalette.Base)
+            self.scaleFactor = 1.0
+
+            self.ui.scrollAreaLeft.setVisible(True)
+            self.ui.actionFit_to_Window.setEnabled(True)
+            self.update_actions()
+
+            if not self.ui.actionFit_to_Window.isChecked():
+                self.ui.imageLabelLeft.adjustSize()
+
+    def update_actions(self):
+        self.ui.actionZoom_In.setEnabled(not self.ui.actionFit_to_Window.isChecked())
+        self.ui.actionZoom_Out.setEnabled(not self.ui.actionFit_to_Window.isChecked())
+        self.ui.actionNormal_Size.setEnabled(not self.ui.actionFit_to_Window.isChecked())
 
 
 if __name__ == "__main__":
